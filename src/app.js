8014801a140ba76a193d62b097fdc6d40d539619
Maximiliano Dalla Porta <gigamax13@gmail.com>
2019-10-29 16:51:48 -0300

    adiciona descricao dos middlewares

--- src/app.js
+++ src/app.js
@@ -1,27 +1,44 @@
-import { config } from 'dotenv';
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import compression from 'compression';
 
+import Environment from './config/Environment';
 import Authors from './routes/Authors';
-
-config();
 
 const {
   BODY_LIMIT,
   PORT,
-} = process.env;
+} = Environment;
 
 const app = express();
 
 app.disable('x-powered-by');
 
 /* Express utilities */
+/*
+* Helmet helps you secure your Express apps by setting various HTTP headers.
+* */
 app.use(helmet());
+
+/*
+* CORS is a node.js package for providing a Connect/Express middleware that can
+* be used to enable CORS with various options.
+* */
 app.use(cors());
+
+/*
+*  Node.js compression middleware.
+* */
 app.use(compression());
+
+/*
+* Node.js body parsing middleware.
+*
+* Parse incoming request bodies in a middleware before your handlers,
+* available under the req.body property.
+* */
 app.use(bodyParser.json({
   limit: BODY_LIMIT,
 }));
